95b0d5f9270ad7b9c3f118c9982e7bd1e0ab7b51
Dan Delzell <dan@delzell.net>
2012-03-04 12:44:03 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Obs::Application.config.secret_token = '8885eb769f6257554ea0ba44299573a9c863194167865d9efbb98cf64a752965dbb1119d4e55ed9ae0296e076ea70d5c397a90c89134918354a5f4e573b10348'
